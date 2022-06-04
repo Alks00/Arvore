0b40ad2b2e5e11b3550422fc66fa6a6d300c0bf4
Lucas Szczepaniak <szczelucas@hotmail.com>
2022-06-04 18:37:05 -0300

    balanceamento feito(tem problema estranho)

--- arvore.h
+++ arvore.h
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 typedef struct no
 {
     struct no *esq;
     struct no *dir;
-    int chave;   
+    int chave;
+    char info[10];   
 }No;
 
 typedef struct arvore
@@ -20,6 +22,22 @@ Tree* criar_arvore(){
     tree -> raiz = NULL;
     tree -> quant = 0;
     return tree;
+}
+
+void imprimirEmOrdem(No* no){
+    if(no != NULL){
+        if (no -> esq != NULL)
+        {
+            imprimirEmOrdem(no -> esq);
+        }
+
+        printf("%d - %s\n", no -> chave, no -> info);
+
+        if (no -> dir != NULL)
+        {
+            imprimirEmOrdem(no -> dir);
+        }
+    }
 }
 
 int altura(No* no){
@@ -38,19 +56,126 @@ int altura(No* no){
     
 }
 
-void balancear(Tree* tree){
+int fator_balanceamento(No* no){
+    int fator = 0;
 
+    if(no -> esq){
+        fator += altura(no -> esq);
+    }
+
+    if(no -> dir){
+        fator -= altura(no -> dir);
+    }
+
+    return fator;
 }
 
-void adicionar(Tree* tree,int valor){
+No* rotacionarEsqEsq(No* no){
+    No* aux = no;
+    No* esq = aux -> esq;
+
+    aux -> esq = esq -> dir;
+    esq -> dir = aux;
+
+    return esq;
+}
+
+No* rotacionarEsqDir(No* no){
+    No* aux = no;
+    No* esq = aux -> esq;
+    No* dir = esq -> dir;
+
+    aux -> esq = dir -> dir;
+    esq -> dir = dir -> esq;
+    dir -> esq = esq;
+    dir -> dir = aux;
+}
+
+No* rotacionarDirEsq(No* no){
+    No* aux = no;
+    No* dir = aux -> dir;
+    No* esq = dir -> esq;
+
+    aux -> dir = esq -> esq;
+    dir -> esq = esq -> dir;
+    esq -> dir = dir;
+    esq -> esq = aux;
+
+    return esq;
+}
+
+No* rotacionarDirDir(No* no){
+    No* aux = no;
+    No* dir = aux -> dir;
+
+    aux -> dir = dir -> esq;
+    dir -> esq = aux;
+
+    return dir;
+}
+
+No* balancear_No(No* no){
+    No* noBalanceado = NULL;
+
+    if (no -> esq)
+    {
+        no -> esq = balancear_No(no -> esq);
+    }
+
+    if (no -> dir)
+    {
+        no -> dir = balancear_No(no -> dir);
+    }
+
+    int fb = fator_balanceamento(no);
+    
+    if (fb >=2)
+    {
+        if (fator_balanceamento(no -> esq) <= -1)
+        {
+            noBalanceado = rotacionarEsqDir(no);
+        }else
+        {
+            noBalanceado = rotacionarEsqEsq(no);
+        }
+    }else if (fb <= -2)
+    {
+        if (fator_balanceamento(no -> dir) >= 1)
+        {
+            noBalanceado = rotacionarDirEsq(no);
+        }else
+        {
+            noBalanceado = rotacionarDirDir(no);
+        }
+    }else
+    {
+        noBalanceado = no;
+    }
+    
+    return noBalanceado;
+}
+
+void balancear(Tree* tree){
+    No * nova_raiz = NULL;
+
+	nova_raiz = balancear_No (tree -> raiz);
+
+	if (nova_raiz != tree -> raiz){
+		tree -> raiz = nova_raiz;
+	}
+}
+
+void adicionar(Tree* tree,int valor,char *info){
     no* proxNo = NULL;
     no* aux = NULL;
 
 
     No* no = (No*)malloc(sizeof(No));
+
     no -> chave = valor;
     no -> dir = NULL;
     no -> esq = NULL;
+    strcpy(no -> info,info);
 
     if (tree -> raiz == NULL)
     {
@@ -84,7 +209,5 @@ void adicionar(Tree* tree,int valor){
         }
         
         balancear(tree);
-    }
-    
-    
+    }  
 }
